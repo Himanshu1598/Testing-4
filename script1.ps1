99afbb4bdc78ecb4fefb0c15f89e47fbe05e32b8
Himanshu1598 <64984897+Himanshu1598@users.noreply.github.com>
2025-02-08 10:48:20 +0530

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,3 +1,3 @@
 Write-Output "Running Script 1"
-Write-Output "ServiceAccountPassword: $CellName"
-Write-Output "ServiceAccountUserName: $AnotherVar"
+Write-Output "ServiceAccountPassword: $ServiceAccountPassword"
+Write-Output "ServiceAccountUserName: $ServiceAccountUserName"
