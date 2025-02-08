c70091fc64000a28baeab6dffc311e5cabebb9f4
Himanshu1598 <64984897+Himanshu1598@users.noreply.github.com>
2025-02-08 22:09:23 +0530

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,3 +1,4 @@
 Write-Output "Running Script 1"
 Write-Output "ServiceAccountPassword: $ServiceAccountPassword"
 Write-Output "ServiceAccountUserName: $ServiceAccountUserName"
+Write-Output "SqlPackagePath: $SqlPackagePath"
